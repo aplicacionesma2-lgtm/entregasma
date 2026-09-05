e5bc3904d9f0fde41a7f0656df5e025299ed1c0d
aplicacionesma2-lgtm <aplicacionesma2@gmail.com>
2026-09-05 12:42:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.markdown(
     .app-header h1 {
         font-size: 2.5rem;
         font-weight: 700;
-        color: #1B2A38;
+        color: #1D4ED8;
         margin: 0;
     }
     .app-subtitle {
